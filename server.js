e6f27fb2f5369e506c329a068904bf33c83289fe
Benefig <benefic2023@gmail.com>
2026-03-22 11:01:38 +0700

    vercel

--- server.js
+++ server.js
@@ -34,7 +34,15 @@ app.set('query parser', 'extended');
 
 const PORT=process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+const server = app.listen(
+    PORT, 
+    console.log(
+        'Server running in ', 
+        process.env.NO, 
+        'mode on port ', 
+        "on" + process.env.HOST + " :" + PORT
+    )
+);
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err,promise)=>{
